fcf9b961cd1d1340f5c895d2321beb21ddf3fb2f
Alex Markley <alex@alexmarkley.com>
2017-10-29 08:38:28 -0400

    consistent indenting

--- src/FaceTracker.cpp
+++ src/FaceTracker.cpp
@@ -35,63 +35,63 @@ FaceTracker::FaceTracker(string myClassifierFileName, FrameDerivatives *myFrameD
 
 TrackerState FaceTracker::processCurrentFrame(void) {
 	if(trackerState == DETECTING || trackerState == LOST || trackerState == STALE) {
-        classificationBoxSet = false;
+		classificationBoxSet = false;
 		std::vector<Rect> faces;
 		Mat classificationFrame = frameDerivatives->getClassificationFrame();
 		double classificationFrameArea = (double)classificationFrame.size().area();
 		double maxFaceSize = sqrt(classificationFrameArea * maxFaceSizePercentage);
-        cascadeClassifier.detectMultiScale(classificationFrame, faces, 1.1, 3, 0|CASCADE_SCALE_IMAGE, Size(maxFaceSize, maxFaceSize));
+		cascadeClassifier.detectMultiScale(classificationFrame, faces, 1.1, 3, 0|CASCADE_SCALE_IMAGE, Size(maxFaceSize, maxFaceSize));
 
-        int largestFace = -1;
-        int largestFaceArea = -1;
-        for( size_t i = 0; i < faces.size(); i++ ) {
-            if(faces[i].area() > largestFaceArea) {
-                largestFace = i;
-                largestFaceArea = faces[i].area();
-            }
-        }
-        if(largestFace >= 0) {
-            classificationBoxSet = true;
+		int largestFace = -1;
+		int largestFaceArea = -1;
+		for( size_t i = 0; i < faces.size(); i++ ) {
+			if(faces[i].area() > largestFaceArea) {
+				largestFace = i;
+				largestFaceArea = faces[i].area();
+			}
+		}
+		if(largestFace >= 0) {
+			classificationBoxSet = true;
 			double classificationScaleFactor = frameDerivatives->getClassificationScaleFactor();
-            classificationBox = faces[largestFace];
-            classificationBoxNormalSize = Rect(
-                (double)classificationBox.x / classificationScaleFactor,
-                (double)classificationBox.y / classificationScaleFactor,
-                (double)classificationBox.width / classificationScaleFactor,
-                (double)classificationBox.height / classificationScaleFactor);
-            //Switch to TRACKING
-            trackerState = TRACKING;
-            #if (CV_MINOR_VERSION < 3)
-            tracker = Tracker::create("KCF");
-            #else
-            tracker = TrackerKCF::create();
-            #endif
-            double trackingBoxWidth = (double)classificationBoxNormalSize.width * trackingBoxPercentage;
-            double trackingBoxHeight = (double)classificationBoxNormalSize.height * trackingBoxPercentage;
-            Rect trackingBox = Rect(
-                (double)classificationBoxNormalSize.x + (((double)classificationBoxNormalSize.width - trackingBoxWidth) / 2.0),
-                (double)classificationBoxNormalSize.y + (((double)classificationBoxNormalSize.height - trackingBoxHeight) / 2.0),
-                trackingBoxWidth,
-                trackingBoxHeight);
-            tracker->init(frameDerivatives->getCurrentFrame(), trackingBox);
-            staleCounter = opticalTrackStaleFramesInterval;
-        }
-    } else if(trackerState == TRACKING) {
-        bool trackSuccess = tracker->update(frameDerivatives->getCurrentFrame(), trackingBox);
-        if(!trackSuccess) {
-            fprintf(stderr, "FaceTracker WARNING! Track lost. Will keep searching...\n");
-            trackingBoxSet = false;
-            trackerState = LOST;
+			classificationBox = faces[largestFace];
+			classificationBoxNormalSize = Rect(
+				(double)classificationBox.x / classificationScaleFactor,
+				(double)classificationBox.y / classificationScaleFactor,
+				(double)classificationBox.width / classificationScaleFactor,
+				(double)classificationBox.height / classificationScaleFactor);
+			//Switch to TRACKING
+			trackerState = TRACKING;
+			#if (CV_MINOR_VERSION < 3)
+			tracker = Tracker::create("KCF");
+			#else
+			tracker = TrackerKCF::create();
+			#endif
+			double trackingBoxWidth = (double)classificationBoxNormalSize.width * trackingBoxPercentage;
+			double trackingBoxHeight = (double)classificationBoxNormalSize.height * trackingBoxPercentage;
+			Rect trackingBox = Rect(
+				(double)classificationBoxNormalSize.x + (((double)classificationBoxNormalSize.width - trackingBoxWidth) / 2.0),
+				(double)classificationBoxNormalSize.y + (((double)classificationBoxNormalSize.height - trackingBoxHeight) / 2.0),
+				trackingBoxWidth,
+				trackingBoxHeight);
+			tracker->init(frameDerivatives->getCurrentFrame(), trackingBox);
+			staleCounter = opticalTrackStaleFramesInterval;
+		}
+	} else if(trackerState == TRACKING) {
+		bool trackSuccess = tracker->update(frameDerivatives->getCurrentFrame(), trackingBox);
+		if(!trackSuccess) {
+			fprintf(stderr, "FaceTracker WARNING! Track lost. Will keep searching...\n");
+			trackingBoxSet = false;
+			trackerState = LOST;
 			//Attempt to re-process this same frame in LOST mode.
-            return this->processCurrentFrame();
-        } else {
-            trackingBoxSet = true;
-            staleCounter--;
-            if(staleCounter <= 0) {
-                trackerState = STALE;
-            }
-        }
-    }
+			return this->processCurrentFrame();
+		} else {
+			trackingBoxSet = true;
+			staleCounter--;
+			if(staleCounter <= 0) {
+				trackerState = STALE;
+			}
+		}
+	}
 	return trackerState;
 }
 
