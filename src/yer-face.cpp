460e2f6cb7b7e559dbebbfff1799cdf7cd0cccab
Alex Markley <alex@alexmarkley.com>
2017-12-01 13:00:19 -0500

    split capture/worker loop and render/event loop up into two threads... currently a LOT of weird behaviors exist because none of the classes are properly thread-safe

--- src/yer-face.cpp
+++ src/yer-face.cpp
@@ -30,6 +30,7 @@ String prev_imgseq;
 String window_name = "Performance Capture Tests";
 
 SDLWindowRenderer sdlWindowRenderer;
+SDL_Thread *workerThread;
 
 Logger *logger;
 SDLDriver *sdlDriver;
@@ -37,8 +38,14 @@ FrameDerivatives *frameDerivatives;
 FaceTracker *faceTracker;
 FaceMapper *faceMapper;
 Metrics *metrics;
-unsigned long frameNum = 0;
 
+//VARIABLES PROTECTED BY frameMetadataMutex
+unsigned long workingFrameNum = 0, completedFrameNum = -1;
+Size frameSize;
+bool frameSizeValid = false;
+SDL_mutex *frameMetadataMutex;
+
+static int runCaptureLoop(void *ptr);
 void doRenderPreviewFrame(void);
 
 int main(int argc, const char** argv) {
@@ -66,10 +73,6 @@ int main(int argc, const char** argv) {
 	dlib_shape_predictor = parser.get<string>("dlib_shape_predictor");
 	prev_imgseq = parser.get<string>("prev_imgseq");
 
-	//A couple of OpenCV classes.
-	VideoCapture capture;
-	Mat frame;
-
 	//Instantiate our classes.
 	frameDerivatives = new FrameDerivatives();
 	sdlDriver = new SDLDriver(frameDerivatives);
@@ -77,60 +80,45 @@ int main(int argc, const char** argv) {
 	faceMapper = new FaceMapper(sdlDriver, frameDerivatives, faceTracker);
 	metrics = new Metrics("YerFace", true);
 
-	//Open the video stream.
-	capture.open(capture_file);
-	if(!capture.isOpened()) {
-		logger->error("Failed opening video stream");
-		return 1;
-	}
-
 	sdlWindowRenderer.window = NULL;
 	sdlWindowRenderer.renderer = NULL;
 
+	//Create locks.
+	if((frameMetadataMutex = SDL_CreateMutex()) == NULL) {
+		throw runtime_error("Failed creating mutex!");
+	}
+
+	//Create worker thread.
+	if((workerThread = SDL_CreateThread(runCaptureLoop, "CaptureLoop", (void *)NULL)) == NULL) {
+		throw runtime_error("Failed spawning worker thread!");
+	}
+
+	//Launch event / rendering loop.
 	while(sdlDriver->getIsRunning()) {
-		bool isPaused = sdlDriver->getIsPaused();
-		if(!isPaused) {
-			capture.read(frame);
-
-			if(sdlWindowRenderer.window == NULL) {
-				Size frameSize = frame.size();
-				sdlWindowRenderer = sdlDriver->createPreviewWindow(frameSize.width, frameSize.height);
+		if(sdlWindowRenderer.window == NULL) {
+			if(SDL_LockMutex(frameMetadataMutex) != 0) {
+				throw runtime_error("Failed locking mutex!");
 			}
-
-			// Start timer
-			metrics->startClock();
-			frameNum++;
-
-			if(frame.empty()) {
-				logger->error("Breaking on no frame ready...");
-				break;
+			if(!frameSizeValid) {
+				SDL_UnlockMutex(frameMetadataMutex);
+				continue;
 			}
-
-			frameDerivatives->setCurrentFrame(frame);
-			faceTracker->processCurrentFrame();
-			faceMapper->processCurrentFrame();
-			metrics->endClock();
+			sdlWindowRenderer = sdlDriver->createPreviewWindow(frameSize.width, frameSize.height);
+			SDL_UnlockMutex(frameMetadataMutex);
 		}
 
 		doRenderPreviewFrame();
 
-		if(!isPaused) {
-			//If requested, write image sequence.
-			if(prev_imgseq.length() > 0) {
-				int filenameLength = prev_imgseq.length() + 32;
-				char filename[filenameLength];
-				snprintf(filename, filenameLength, "%s-%06lu.png", prev_imgseq.c_str(), frameNum);
-				logger->debug("YerFace writing preview frame to %s ...", filename);
-				imwrite(filename, frameDerivatives->getPreviewFrame());
-			}
-		}
-
 		sdlDriver->doHandleEvents();
 
-		if(isPaused) {
-			SDL_Delay(50);
-		}
+		SDL_Delay(10);
 	}
+
+	//Join worker thread.
+	SDL_WaitThread(workerThread, NULL);
+
+	//Cleanup.
+	SDL_DestroyMutex(frameMetadataMutex);
 
 	delete metrics;
 	delete faceMapper;
@@ -138,6 +126,74 @@ int main(int argc, const char** argv) {
 	delete frameDerivatives;
 	delete sdlDriver;
 	delete logger;
+	return 0;
+}
+
+int runCaptureLoop(void *ptr) {
+	//A couple of OpenCV classes.
+	VideoCapture capture;
+	Mat frame;
+
+	//Open the video stream.
+	capture.open(capture_file);
+	if(!capture.isOpened()) {
+		logger->error("Failed opening video stream");
+		sdlDriver->setIsRunning(false);
+		return -1;
+	}
+
+	while(sdlDriver->getIsRunning()) {
+		if(!sdlDriver->getIsPaused()) {
+
+			if(!capture.read(frame)) {
+				logger->info("Capture is finished?");
+				sdlDriver->setIsRunning(false);
+				continue;
+			}
+
+			if(SDL_LockMutex(frameMetadataMutex) != 0) {
+				throw runtime_error("Failed locking mutex!");
+			}
+			workingFrameNum++;
+			if(!frameSizeValid) {
+				frameSize = frame.size();
+				frameSizeValid = true;
+			}
+			if(frame.empty()) {
+				logger->error("Breaking on no frame ready...");
+				SDL_UnlockMutex(frameMetadataMutex);
+				sdlDriver->setIsRunning(false);
+				return -1;
+			}
+			SDL_UnlockMutex(frameMetadataMutex);
+
+			// Start timer
+			metrics->startClock();
+
+			frameDerivatives->setCurrentFrame(frame);
+			faceTracker->processCurrentFrame();
+			faceMapper->processCurrentFrame();
+
+			metrics->endClock();
+
+			if(SDL_LockMutex(frameMetadataMutex) != 0) {
+				throw runtime_error("Failed locking mutex!");
+			}
+			completedFrameNum = workingFrameNum;
+			SDL_UnlockMutex(frameMetadataMutex);
+
+			//If requested, write image sequence.
+			if(prev_imgseq.length() > 0) {
+				int filenameLength = prev_imgseq.length() + 32;
+				char filename[filenameLength];
+				snprintf(filename, filenameLength, "%s-%06lu.png", prev_imgseq.c_str(), completedFrameNum);
+				logger->debug("YerFace writing preview frame to %s ...", filename);
+				imwrite(filename, frameDerivatives->getPreviewFrame());
+			}
+		}
+	}
+	capture.release();
+
 	return 0;
 }
 
